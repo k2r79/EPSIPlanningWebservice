9a658ae2c1cf981bf78d65fc36ea2ecec33ada81
Vincent Kelleher <k2r79@hotmail.fr>
2015-11-30 17:50:48 +0100

    Debug OS variable

--- server.py
+++ server.py
@@ -38,4 +38,4 @@ def parse_cours(donnees):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=os.environ.get("DEBUG", True))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=os.environ.get("DEBUG", False))
