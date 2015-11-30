823ca09d81af920b3f3c4d09f904338d43e81e5d
Vincent Kelleher <k2r79@hotmail.fr>
2015-11-30 17:49:14 +0100

    Next lesson implemented

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask.ext.pymongo import PyMongo
 from flask.ext.cors import CORS
 import os
+import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -9,17 +10,32 @@ CORS(app)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
+
 @app.route("/cours/")
 def cours():
+    cours = parse_cours(mongo.db.planning.find())
+
+    return jsonify(data=cours), 200
+
+
+@app.route("/cours/prochain")
+def prochain_cours():
+    cours = parse_cours(mongo.db.planning.find({"horaire_debut": {"$gte": datetime.datetime.utcnow()}}).sort("horaire_debut").limit(1))
+
+    return jsonify(data=cours), 200
+
+
+def parse_cours(donnees):
     cours = []
-    for planning in mongo.db.planning.find():
+    for planning in donnees:
         planning["_id"] = str(planning["_id"])
         planning["horaire_debut"] = planning["horaire_debut"].isoformat()
         planning["horaire_fin"] = planning["horaire_fin"].isoformat()
 
         cours.append(planning)
 
-    return jsonify(data=cours), 200
+    return cours
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=os.environ.get("DEBUG", True))
